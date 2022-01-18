bd459927ce7b596641fd61e3145ea180b79fa61d
JovanaRobinson <jr951@live.mdx.ac.uk>
2022-01-18 13:06:15 +0000

    added two alternates for scrolling

---
+++ Fadecandy files/Fadecandy_2400-main/examples/basic_animations.py
@@ -0,0 +1,27 @@
+import opc
+import time
+import random
+
+leds = [(255,255,255)]*360 #white
+
+client = opc.Client('localhost:7890')
+client.put_pixels(leds)
+client.put_pixels(leds)
+
+#for led in leds: # pick out an element: led = (255,255,255)
+for led in range(60): #pick out indices: led = 0,1,2,3....
+    leds[led] = (255,0,0)
+    time.sleep(.1)
+    client.put_pixels(leds)
+
+led = 0
+while led<60:
+    leds[led] = (0,255,0)
+    time.sleep(.1)
+    client.put_pixels(leds)
+    led = led + 1 #or reverse if you want
+    
+
+
+
+
