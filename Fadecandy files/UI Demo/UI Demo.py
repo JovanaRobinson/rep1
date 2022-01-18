25c192658d91ecc9d33c69f2db90d91c49aa07cb
JovanaRobinson <jr951@live.mdx.ac.uk>
2022-01-18 12:05:14 +0000

    loop for datatype validation
    check if data is int

--- Fadecandy files/UI Demo/UI Demo.py
+++ Fadecandy files/UI Demo/UI Demo.py
@@ -1,9 +1,15 @@
-value = input("Welcome to the menu. Options are listed below:")
+value = input("Welcome to the menu. Options are listed below: \n\t 1. Roll \n\t 2. Sweep \n\t 3. Scroll \n Type the number of your choice and press enter:") #\n - newline; \t - tab
 
-print("The value you input is:", value)
-print(f'it is of type {type(value)}.')
+#print("The value you input is:", value)
+#print(f'it is of type {type(value)}.')
 
-value = int(value)
+while True:
+    if value.isdigit() == True: # .isdigit()
+         value = int(value)
+         break # on correct value datatype: exit the loop
+    else:
+        value = input("invalid input, please provide an integer:") #ask for a new value
 
-print("The converted value is:", value)
-print(f'it is of type {type(value)}.')
+#print("The converted value is:", value)
+#print(f'it is of type {type(value)}.')
+    
