2d9720137e5a694c78499a931ac3cfd0d78cece8
JovanaRobinson <jr951@live.mdx.ac.uk>
2022-02-18 23:25:08 +0000

    Snake Pattern

--- Fadecandy files/Fadecandy_2400-main/examples/basic_animations.py
+++ Fadecandy files/Fadecandy_2400-main/examples/basic_animations.py
@@ -9,17 +9,30 @@ client.put_pixels(leds)
 client.put_pixels(leds)
 
 #for led in leds: # pick out an element: led = (255,255,255)
-for led in range(60): #pick out indices: led = 0,1,2,3....
-    leds[led] = (255,0,0)
-    time.sleep(.1)
-    client.put_pixels(leds)
+#for led in range(60): #pick out indices: led = 0,1,2,3....
+#   leds[led] = (255,0,0)
+#   time.sleep(.1)
+#   client.put_pixels(leds)
 
-led = 0
-while led<60:
-    leds[led] = (0,255,0)
-    time.sleep(.1)
-    client.put_pixels(leds)
-    led = led + 1 #or reverse if you want
+#led = 0
+#while led<60:
+#    leds[led] = (0,255,0)
+#    time.sleep(.1)
+#    client.put_pixels(leds)
+#    led = led + 1 #or reverse if you want
+
+while True:
+    for led in range(0, 360, 60):
+        leds = [(255,255,255)]*360
+        leds[355-led] = (0,0,255)
+        leds[355-led+1] = (0,0,255)
+        leds[355-led+2] = (0,0,255)
+        leds[355-led+3] = (0,0,255)
+        leds[355-led+4] = (0,0,255)
+        if led == 355:
+            led = 0
+        client.put_pixels(leds)
+        time.sleep(0.02)
     
 
 
